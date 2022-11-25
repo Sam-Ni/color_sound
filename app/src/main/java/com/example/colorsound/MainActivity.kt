b03565073993fca898051b3916d88020e4b59e52
Lisery <1143765064@qq.com>
2022-11-25 18:51:53 +0800

    顶部栏颜色修改成和背景一样

--- app/src/main/java/com/example/colorsound/MainActivity.kt
+++ app/src/main/java/com/example/colorsound/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.colorsound
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
 import com.example.colorsound.ui.ColorSoundApp
 import com.example.colorsound.ui.theme.ColorSoundTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ColorSoundTheme {
+                val systemUiController = rememberSystemUiController()
+                systemUiController.setStatusBarColor(
+                    MaterialTheme.colorScheme.background,
+                    darkIcons = !isSystemInDarkTheme()
+                )
                 ColorSoundApp()
             }
         }
